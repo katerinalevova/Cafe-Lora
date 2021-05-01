3268925f0a4e214d4662648127160295037ff5bb
Kateřina Levová <katerina.levova@gmail.com>
2021-05-01 12:16:16 +0200

    Plneni hrnecku na kliknuti

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css';
 
 console.log('funguju!');
 
+//Zobrazeni a schovani navigace v menu
+
 const zobrazeniNavigace = () => {
   document.querySelector('nav').classList.toggle('nav-closed');
 };
@@ -16,3 +18,21 @@ const vyberPrvekzNavigace = document.querySelectorAll('nav a');
 for (let i = 0; i < vyberPrvekzNavigace.length; i += 1) {
   vyberPrvekzNavigace[i].addEventListener('click', schovaniNavigace);
 }
+
+//Plneni hrnecku
+let ordered = 'false';
+
+const plnitHrnecek = () => {
+  if (ordered === 'false') {
+    document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+    document.querySelector('.order-btn').textContent = 'Zrušit';
+    ordered = 'true';
+  } else {
+    document
+      .querySelector('.drink__cup')
+      .classList.remove('drink__cup--selected');
+    document.querySelector('.order-btn').textContent = 'Objednat';
+    ordered = 'false';
+  }
+};
+document.querySelector('.order-btn').addEventListener('click', plnitHrnecek);
