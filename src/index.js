fc635dc778bbbd864ebffbed343309593c7f1d65
Kateřina Levová <katerina.levova@gmail.com>
2021-05-01 12:04:23 +0200

    Zobrazeni a schovani navigace na kliknuti

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
 
 console.log('funguju!');
+
+const zobrazeniNavigace = () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+};
+
+const schovaniNavigace = () => {
+  document.querySelector('nav').classList.add('nav-closed');
+};
+
+document.querySelector('#nav-btn').addEventListener('click', zobrazeniNavigace);
+
+const vyberPrvekzNavigace = document.querySelectorAll('nav a');
+for (let i = 0; i < vyberPrvekzNavigace.length; i += 1) {
+  vyberPrvekzNavigace[i].addEventListener('click', schovaniNavigace);
+}
